525f55e007a2a9a41203b7e8d61862085854e65b
EzekielCagas <145763269+EzekielCagas@users.noreply.github.com>
2023-11-08 18:52:00 +0800

    potatong inaaaaa

---
+++ Dressire/app/src/main/java/com/example/dressire/Highlight.java
@@ -0,0 +1,16 @@
+package com.example.dressire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class Highlight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highlight);
+    }
+}
